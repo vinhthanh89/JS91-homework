06fcb637723561c0597dd75568c5b48dbec48306
vinhthanh89 <vinhthanh89@gmail.com>
2024-01-13 22:54:44 +0700

    abc

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,6 +7,7 @@ import ThemeContext from "../../Contexts/ThemeContext";
 
 const Header = () => {
   const { theme, setTheme } = useContext(ThemeContext);
+  console.log(theme);
 
   return (
     <div className="header">
